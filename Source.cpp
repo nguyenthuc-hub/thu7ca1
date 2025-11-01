d5a3bf1116b7090cfa25455ebd0fc97f0b1e692e
nguyenthuc-hub <nguyenthuc123.py@gmail.com>
2025-11-01 09:08:18 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b);
+int hieu(int a, int b) {
+	return a - b;
+}
+int tong(int a, int b){
+	return a + b;
+}
+int tich(int a, int b){
+	return a * b ;
+}
 float thuong(int a, int b);
+
+boolean sochinhphuong (int a);
+
+float thuong(int a, int b) {
+	if (b != 0) {
+	 return -1;
+	}
+	return a/b;
+}
+bool checkChanLe(int n){
+	if(n%2==0)
+		cout<<n<<" la so chan."
+	else cout<<n<<" la so le."
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
